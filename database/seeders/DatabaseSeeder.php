0207809d1472a67c6453336fb82e825969b88045
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-08-03 19:23:30 -0500

    feat(laravel): factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,7 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(PostsTableSeeder::class);
+        // $this->call(PostsTableSeeder::class);
+        
+        Post::factory(100)->create();
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
